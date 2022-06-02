e9f45c28855a23100003ca92a3ee1739f77b2a89
Hannah Yi <hannahyi@Hannahs-MBP.fios-router.home>
2022-06-01 22:45:17 -0400

    first commit

---
+++ alphabetize.js
@@ -0,0 +1,134 @@
+const moderateLoop = ["Frasier", "Philip", "Paloma", "Gran", "Eldin", "Karina", "Tiffy", "Rosemaria", "Ula", "Donny", "Theadora", "Else", "Merilyn", "Minnie", "Wilbert", "Jillayne", "Essa", "Justine", "Rakel", "Crosby", "Abbott", "Rosalia", "Bert", "Ceil", "Peyton", "Brittan", "Tamar", "Paolina", "Paula", "Alfonse", "Cal", "Bear", "Glen", "Malissia", "Marla", "Iggy", "Bryce", "Lamar", "Inglis", "Laurie", "Jacinthe", "Lissy", "Olympia", "Chad", "Trixie", "Erina", "Dael", "Cristie", "Isidore", "Artair", "Caesar", "Far", "Dawna", "Lucky", "Spike", "Gabriella", "Carine", "Beverie", "Sunny", "Lois", "Kaspar", "Nate", "Charlot", "Constantina", "Jandy", "Michele", "Edy", "Erin", "Georgina", "Avrit", "Edyth", "Lynnett", "Corly", "Rodge", "Tomaso", "Eli", "Emelina", "Putnem", "Jo", "Rodd", "Aldin", "Seline", "Leese", "Dotty", "Tomasina", "Charlton", "Michaela", "Carlin", "Kakalina", "Leonelle", "Lela", "Eve", "Alasteir", "Paula", "Jean", "Tadeo", "Harbert", "Kyla", "Waylin", "Nerta"];
+
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='A') {
+    let aLetter = [moderateLoop[i][0]=='A']
+    console.log(aLetter.length+' word\(s\) that start with \'A\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='B') { 
+    let bLetter = [moderateLoop[i][0]=='B']
+    console.log(bLetter.length+' word\(s\) that start with \'B\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='C') { 
+    let cLetter = [moderateLoop[i][0]=='C']
+    console.log(cLetter.length+' word\(s\) that start with \'C\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='D') { 
+    let dLetter = [moderateLoop[i][0]=='D']
+    console.log(dLetter.length+' word\(s\) that start with \'D\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='E') { 
+    let eLetter = [moderateLoop[i][0]=='E']
+    console.log(eLetter.length+' word\(s\) that start with \'E\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='F') { 
+    let fLetter = [moderateLoop[i][0]=='F']
+    console.log(fLetter.length+' word\(s\) that start with \'F\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='G') { 
+    let gLetter = [moderateLoop[i][0]=='G']
+    console.log(gLetter.length+' word\(s\) that start with \'G\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='H') { 
+    let hLetter = [moderateLoop[i][0]=='H']
+    console.log(hLetter.length+' word\(s\) that start with \'H\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='I') { 
+    let iLetter = [moderateLoop[i][0]=='I']
+    console.log(iLetter.length+' word\(s\) that start with \'I\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='J') { 
+    let jLetter = [moderateLoop[i][0]=='J']
+    console.log(jLetter.length+' word\(s\) that start with \'J\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='K') { 
+    let kLetter = [moderateLoop[i][0]=='K']
+    console.log(kLetter.length+' word\(s\) that start with \'K\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='L') { 
+    let lLetter = [moderateLoop[i][0]=='L']
+    console.log(lLetter.length+' word\(s\) that start with \'L\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='M') { 
+    let mLetter = [moderateLoop[i][0]=='M']
+    console.log(mLetter.length+' word\(s\) that start with \'M\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='N') { 
+    let nLetter = [moderateLoop[i][0]=='N']
+    console.log(nLetter.length+' word\(s\) that start with \'N\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='O') { 
+    let oLetter = [moderateLoop[i][0]=='O']
+    console.log(oLetter.length+' word\(s\) that start with \'O\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='P') { 
+    let pLetter = [moderateLoop[i][0]=='P']
+    console.log(pLetter.length+' word\(s\) that start with \'P\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='Q') { 
+    let qLetter = [moderateLoop[i][0]=='Q']
+    console.log(qLetter.length+' word\(s\) that start with \'Q\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='R') { 
+    let rLetter = [moderateLoop[i][0]=='R']
+    console.log(rLetter.length+' word\(s\) that start with \'R\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='S') { 
+    let sLetter = [moderateLoop[i][0]=='S']
+    console.log(sLetter.length+' word\(s\) that start with \'S\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='T') { 
+    let tLetter = [moderateLoop[i][0]=='T']
+    console.log(tLetter.length+' word\(s\) that start with \'T\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='U') { 
+    let uLetter = [moderateLoop[i][0]=='U']
+    console.log(uLetter.length+' word\(s\) that start with \'U\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='V') { 
+    let vLetter = [moderateLoop[i][0]=='V']
+    console.log(vLetter.length+' word\(s\) that start with \'V\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='W') { 
+    let wLetter = [moderateLoop[i][0]=='W']
+    console.log(wLetter.length+' word\(s\) that start with \'W\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='X') { 
+    let xLetter = [moderateLoop[i][0]=='X']
+    console.log(xLetter.length+' word\(s\) that start with \'X\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='Y') { 
+    let yLetter = [moderateLoop[i][0]=='Y']
+    console.log(yLetter.length+' word\(s\) that start with \'Y\':', moderateLoop[i]);
+}}
+for (let i=0; i<moderateLoop.length; i++) {
+if (moderateLoop[i][0]=='Z') { 
+    let zLetter = [moderateLoop[i][0]=='Z']
+    console.log(zLetter.length+' word\(s\) that start with \'Z\':', moderateLoop[i]);
+}}
+// console.log(aLetter[i],bLetter[i],cLetter,dLetter,eLetter,fLetter,gLetter,hLetter,iLetter,jLetter,kLetter,lLetter,mLetter,nLetter,oLetter,pLetter,qLetter,rLetter,sLetter,tLetter,uLetter,vLetter,wLetter,xLetter,yLetter,zLetter)
+// +moderateLoop[i][0]=='B'+moderateLoop[i][0]=='C'+moderateLoop[i][0]=='D'+moderateLoop[i][0]=='E'+moderateLoop[i][0]=='F'+moderateLoop[i][0]=='G'+moderateLoop[i][0]=='H'+moderateLoop[i][0]=='I'+moderateLoop[i][0]=='J'+moderateLoop[i][0]=='K'+moderateLoop[i][0]=='L'+moderateLoop[i][0]=='M'+moderateLoop[i][0]=='N'+moderateLoop[i][0]=='O'+moderateLoop[i][0]=='P'+moderateLoop[i][0]=='Q'+moderateLoop[i][0]=='R'+moderateLoop[i][0]=='S'+moderateLoop[i][0]=='T'+moderateLoop[i][0]=='U'+moderateLoop[i][0]=='V'+moderateLoop[i][0]=='W'+moderateLoop[i][0]=='X'+moderateLoop[i][0]=='Y'+moderateLoop[i][0]=='Z') {
